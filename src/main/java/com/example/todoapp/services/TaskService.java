838a0766432ff7be2f84840d4da99008c661a961
mohamed.moideen <mohamed.moideen@infoedge.com>
2025-01-19 01:21:21 +0530

    To do project .....................................

---
+++ src/main/java/com/example/todoapp/services/TaskService.java
@@ -0,0 +1,44 @@
+package com.example.todoapp.services;
+
+import com.example.todoapp.models.Task;
+import com.example.todoapp.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+       Task task =  taskRepository.findById(id)
+               .orElseThrow(()-> new IllegalArgumentException("Invalid task id"));
+        task.setCompleted(!task.isCompleted());
+       taskRepository.save(task);
+
+    }
+}
